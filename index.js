2f372301ac4492f8a6ec5ba296d9d964564f0dd1
Farah alsoqi <farahmanna97@gmail.com>
2023-10-08 19:21:12 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
+
 }
 
-function renderBooks(books) {
+
+ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -15,3 +19,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
